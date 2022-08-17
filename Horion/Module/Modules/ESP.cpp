63e1a3c49507186dba9313960910bfc66c20c2de
MacroModel2019 <33865334+xlfd2018@users.noreply.github.com>
2022-08-18 00:22:27 +0800

    Update ESP.cpp

--- Horion/Module/Modules/ESP.cpp
+++ Horion/Module/Modules/ESP.cpp
@@ -1,6 +1,7 @@
 #include "ESP.h"
 
 #include "../../../Utils/Target.h"
+#include "../../../Memory/Hooks.h"
 
 ESP::ESP() : IModule('O', Category::VISUAL, "Makes it easier to find entities around you.") {
 	registerBoolSetting("Rainbow", &doRainbow, doRainbow);
@@ -18,6 +19,9 @@ const char* ESP::getModuleName() {
 static float rcolors[4];
 
 void doRenderStuff(C_Entity* ent, bool isRegularEntitie) {
+	if (std::time(nullptr) < g_Hooks.connecttime + 1)
+		return;
+
 	static auto espMod = moduleMgr->getModule<ESP>();
 	
 	C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
@@ -26,6 +30,10 @@ void doRenderStuff(C_Entity* ent, bool isRegularEntitie) {
 	if (ent->timeSinceDeath > 0)
 		return;
 	static auto noFriendsModule = moduleMgr->getModule<NoFriends>();
+
+	if (!ent->checkNameTagFunc())
+		return;
+
 	if (!noFriendsModule->isEnabled() && FriendList::findPlayer(ent->getNameTag()->getText())) {
 		DrawUtils::setColor(0.1f, 0.9f, 0.1f, 1.f);
 	} else if (Target::isValidTarget(ent)) {
@@ -40,8 +48,6 @@ void doRenderStuff(C_Entity* ent, bool isRegularEntitie) {
 		if (ent->isInvisible())
 			return;
 
-		if (!localPlayer->canAttack(ent, false))
-			return;
 		if (espMod->doRainbow)
 			DrawUtils::setColor(rcolors[0], rcolors[1], rcolors[2], 1.f);
 		else
@@ -52,8 +58,6 @@ void doRenderStuff(C_Entity* ent, bool isRegularEntitie) {
 		DrawUtils::draw2D(ent, (float)fmax(0.4f, 1 / (float)fmax(1, localPlayer->eyePos0.dist(ent->eyePos0) * 3.f)));
 	else 
 		DrawUtils::drawEntityBox(ent, (float)fmax(0.2f, 1 / (float)fmax(1, localPlayer->eyePos0.dist(ent->eyePos0))));
-	
-	
 }
 
 void ESP::onPreRender(C_MinecraftUIRenderContext* renderCtx) {
