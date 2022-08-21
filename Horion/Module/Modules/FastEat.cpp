00500465680fe2013023db19ddba4525ccb6187e
MacroModel2019 <33865334+xlfd2018@users.noreply.github.com>
2022-08-21 11:00:14 +0800

    fasteat崩溃修复2

--- Horion/Module/Modules/FastEat.cpp
+++ Horion/Module/Modules/FastEat.cpp
@@ -14,13 +14,18 @@ const char* FastEat::getModuleName() {
 void FastEat::onTick(C_GameMode* gm) {
 	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
 	C_Inventory* inv = supplies->inventory;
-
-	for (int i = 0; i < 36; i++) {
-		C_ItemStack* stack = inv->getItemStack(i);
-		if (stack->item != NULL && (*stack->item)->itemId != 261 && (*stack->item)->getMaxUseDuration(stack) == 32) {
-			(*stack->item)->setMaxUseDuration(duration);
-			items.push_back(*stack->item);
+	if (g_Data.getLocalPlayer() != nullptr && g_Data.isInGame()) {
+		for (int i = 0; i < 36; i++) {
+			C_ItemStack* stack = inv->getItemStack(i);
+			if (stack->item != nulllptr && (*stack->item)->itemId != 261 && (*stack->item)->getMaxUseDuration(stack) > 0/*防止某些服务器的资源包修改食物的MaxUseDuration*/) {
+				if (items.find(*stack->item) == items.end()) {
+					items[*stack->item] = (*stack->item)->getMaxUseDuration(stack);
+				}
+				(*stack->item)->setMaxUseDuration(duration);
+			}
 		}
+	} else {
+		items.clear();//重进服citem指针会变
 	}
 }
 
@@ -28,10 +33,14 @@ void FastEat::onDisable() {
 	if (g_Data.getLocalPlayer() == nullptr)
 		return;
 
-	for (C_Item* i : items) {
-		i->setMaxUseDuration(32);
+	for (auto& i : items) {
+		__try {
+			i.first->setMaxUseDuration(i.second);
+		} __except (EXCEPTION_EXECUTE_HANDLER) {
+			items.clear();
+			return;
+		}
 	}
-
 	items.clear();
 	/*
 	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
