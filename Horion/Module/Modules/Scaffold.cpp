6a51be3fb3750b3636b646616d777fbf6137d772
Coft Anery <AneryCoft13363134145@gmail.com>
2022-08-21 12:57:33 +0800

    自动搭路

--- Horion/Module/Modules/Scaffold.cpp
+++ Horion/Module/Modules/Scaffold.cpp
@@ -1,10 +1,19 @@
 #include "Scaffold.h"
 
-#include "../../../Utils/Logger.h"
+//#include "../../../Utils/Logger.h"
 
 Scaffold::Scaffold() : IModule(VK_NUMPAD1, Category::WORLD, "Automatically build blocks beneath you.") {
-	registerBoolSetting("Spoof", &spoof, spoof);
-	registerBoolSetting("Staircase Mode", &staircaseMode, staircaseMode);
+	mode = (*new SettingEnum(this))
+		.addEntry(EnumEntry("Normal", 0))
+		.addEntry(EnumEntry("Extend", 1))
+		.addEntry(EnumEntry("Staircase", 2))
+		.addEntry(EnumEntry("Horizontal", 3));
+	registerEnumSetting("Mode", &mode, 0);
+	this->registerIntSetting("Extend Length", &this->length, this->length, 1, 10);
+	registerBoolSetting("AutoBlock", &this->autoBlock, this->autoBlock);
+	registerBoolSetting("blockCount", &this->showBlockCount, this->showBlockCount);
+	registerBoolSetting("Rotations", &this->rotations, this->rotations);
+	registerBoolSetting("Render", &this->render, this->render);
 }
 
 Scaffold::~Scaffold() {
@@ -16,6 +25,7 @@ const char* Scaffold::getModuleName() {
 
 bool Scaffold::tryScaffold(vec3_t blockBelow) {
 	blockBelow = blockBelow.floor();
+	renderPos = blockBelow;
 
 	C_Block* block = g_Data.getLocalPlayer()->region->getBlock(vec3_ti(blockBelow));
 	C_BlockLegacy* blockLegacy = (block->blockLegacy);
@@ -49,8 +59,9 @@ bool Scaffold::tryScaffold(vec3_t blockBelow) {
 			i++;
 		}
 		if (foundCandidate) {
-			if (spoof) findBlock();
+			//if (spoof) findBlock();
 			bool idk = true;
+			canRender = true;
 			g_Data.getCGameMode()->buildBlock(&blok, i, idk);
 
 			return true;
@@ -59,8 +70,8 @@ bool Scaffold::tryScaffold(vec3_t blockBelow) {
 	return false;
 }
 
-bool Scaffold::findBlock() {
-	__int64 id = *g_Data.getLocalPlayer()->getUniqueId();
+void Scaffold::findBlock() {
+	/*__int64 id = *g_Data.getLocalPlayer()->getUniqueId();
 	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
 	C_Inventory* inv = supplies->inventory;
 	for (int n = 0; n < 9; n++) {
@@ -75,18 +86,65 @@ bool Scaffold::findBlock() {
 	}
 	C_MobEquipmentPacket a(id, *g_Data.getLocalPlayer()->getSelectedItem(), supplies->selectedHotbarSlot, supplies->selectedHotbarSlot);
 	g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&a);
-	return false;
+	return false;*/
+	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
+	C_Inventory* inv = supplies->inventory;
+	int emptySlot = 0;
+
+	for (int i = 0; i < 36; i++) {
+		C_ItemStack* stack = inv->getItemStack(i);
+		if (i < 9) {
+			if (stack->item != nullptr) {
+				if (stack->getItem()->isBlock()) {
+					supplies->selectedHotbarSlot = i;
+					return;
+				}
+			}
+			else {
+				emptySlot = i;
+			}
+		}
+		else {
+			if (stack->item != nullptr && stack->getItem()->isBlock()) {
+				inv->moveItem(i, emptySlot);
+				supplies->selectedHotbarSlot = emptySlot;
+				return;
+			}
+		}
+	}
 }
 
-void Scaffold::onTick(C_GameMode* gm) {
+void Scaffold::calcCount() {
+	C_PlayerInventoryProxy* supplies = g_Data.getLocalPlayer()->getSupplies();
+	C_Inventory* inv = supplies->inventory;
+
+	blockCount = 0;
+	for (int i = 0; i < 36; i++) {
+		C_ItemStack* stack = inv->getItemStack(i);
+		if (stack->item != nullptr && stack->getItem()->isBlock()) {
+			blockCount += stack->count;
+		}
+	}
+}
+
+void Scaffold::onGetPickRange() {
 	if (g_Data.getLocalPlayer() == nullptr)
 		return;
-	if (!g_Data.canUseMoveKeys())
+	/*if (!g_Data.canUseMoveKeys())
+		return;*/
+
+	if (showBlockCount && blockCount == 0)
 		return;
-	
+
 	auto selectedItem = g_Data.getLocalPlayer()->getSelectedItem();
-	if ((selectedItem == nullptr || selectedItem->count == 0 || selectedItem->item == nullptr || !selectedItem->getItem()->isBlock()) && !spoof)  // Block in hand?
-		return;
+
+	if (selectedItem == nullptr || selectedItem->count == 0 || selectedItem->item == nullptr || !selectedItem->getItem()->isBlock()) {  // Block in hand?
+		if (autoBlock) {
+			findBlock();
+		}
+		else
+			return;
+	}
 
 
 	// Adjustment by velocity
@@ -94,10 +152,64 @@ void Scaffold::onTick(C_GameMode* gm) {
 	vec3_t vel = g_Data.getLocalPlayer()->velocity;
 	vel = vel.normalize();  // Only use values from 0 - 1
 
+	switch (mode.selected) {
+	case 0:
+	{
+		vec3_t blockBelow = g_Data.getLocalPlayer()->eyePos0;  // Block below the player
+		blockBelow.y -= g_Data.getLocalPlayer()->height;
+		blockBelow.y -= 0.5f;
 
+		if (!tryScaffold(blockBelow)) {
+			if (speed > 0.05f) {  // Are we actually walking?
+				blockBelow.z -= vel.z * 0.4f;
+				if (!tryScaffold(blockBelow)) {
+					blockBelow.x -= vel.x * 0.4f;
+					if (!tryScaffold(blockBelow) && g_Data.getLocalPlayer()->isSprinting()) {
+						blockBelow.z += vel.z;
+						blockBelow.x += vel.x;
+						tryScaffold(blockBelow);
+					}
+				}
+			}
+		}
+	}
+	break;
+	case 1:
+	{
+		static int  i = 0;
 
+		float cal = (g_Data.getLocalPlayer()->yaw + 90) * (PI / 180);
+		vec3_t blockBelow = g_Data.getLocalPlayer()->eyePos0;  // Block 1 block below the player
+		blockBelow.y -= g_Data.getLocalPlayer()->height;
 
-	if (staircaseMode) {
+		if (i > length) {
+			i = 0;
+		}
+
+		if (i != 0) {
+			blockBelow.x = blockBelow.x += cos(cal) * i;  // Block 1 ahead the player X
+			blockBelow.z = blockBelow.z += sin(cal) * i;  // Block 1 ahead the player Z
+		}
+
+		if (!tryScaffold(blockBelow)) {
+			if (speed > 0.05f) {  // Are we actually walking?
+				blockBelow.z -= vel.z * 0.4f;
+				if (!tryScaffold(blockBelow)) {
+					blockBelow.x -= vel.x * 0.4f;
+
+					if (!tryScaffold(blockBelow) && g_Data.getLocalPlayer()->isSprinting()) {
+						blockBelow.z += vel.z;
+						blockBelow.x += vel.x;
+						tryScaffold(blockBelow);
+					}
+				}
+			}
+		}
+		++i;
+	}
+	break;
+	case 2:
+	{
 		vec3_t blockBelow = g_Data.getLocalPlayer()->eyePos0;  // Block 1 block below the player
 		blockBelow.y -= g_Data.getLocalPlayer()->height;
 		blockBelow.y -= 1.5f;
@@ -124,10 +236,12 @@ void Scaffold::onTick(C_GameMode* gm) {
 				}
 			}
 		}
-	} else {
+	}
+	break;
+	case 3:
+	{
 		vec3_t blockBelow = g_Data.getLocalPlayer()->eyePos0;  // Block below the player
-		blockBelow.y -= g_Data.getLocalPlayer()->height;
-		blockBelow.y -= 0.5f;
+		blockBelow.y = horizontalHigh;
 
 		if (!tryScaffold(blockBelow)) {
 			if (speed > 0.05f) {  // Are we actually walking?
@@ -143,9 +257,67 @@ void Scaffold::onTick(C_GameMode* gm) {
 			}
 		}
 	}
+	}
 }
 
 void Scaffold::onDisable() {
 	if (g_Data.getLocalPlayer() == nullptr)
 		return;
+
+	g_Data.getLocalPlayer()->getSupplies()->selectedHotbarSlot = prevSlot;
+}
+
+void Scaffold::onEnable() {
+	if (g_Data.getLocalPlayer() == nullptr)
+		return;
+
+	horizontalHigh = g_Data.getLocalPlayer()->eyePos0.y;
+	horizontalHigh -= g_Data.getLocalPlayer()->height;
+	horizontalHigh -= 0.5f;
+	//水平搭路计算脚下高度
+
+	prevSlot = g_Data.getLocalPlayer()->getSupplies()->selectedHotbarSlot;
+}
+
+void Scaffold::onPlayerTick(C_Player* player) {
+	/*
+	player->pitch = angle.x;
+	player->bodyYaw = angle.y;
+	player->yawUnused1 = angle.y;
+	*/
+}
+
+void Scaffold::onSendPacket(C_Packet* packet, bool& cancelSend) {
+	/*
+	if (packet->isInstanceOf<C_MovePlayerPacket>()) {
+		C_MovePlayerPacket* movePacket = reinterpret_cast<C_MovePlayerPacket*>(packet);
+		movePacket->pitch = angle.x;
+		movePacket->headYaw = angle.y;
+		movePacket->yaw = angle.y;
+	}
+	if (packet->isInstanceOf<PlayerAuthInputPacket>()) {
+		PlayerAuthInputPacket* authInputPacket = reinterpret_cast<PlayerAuthInputPacket*>(packet);
+		authInputPacket->pitch = angle.x;
+		authInputPacket->yawUnused = angle.y;
+		authInputPacket->yaw = angle.y;
+	}
+	*/
+}
+
+void Scaffold::onPostRender(C_MinecraftUIRenderContext* renderCtx) {
+	if (g_Data.canUseMoveKeys()) {
+		if (showBlockCount) {
+			calcCount();
+			vec2_t windowSize = g_Data.getClientInstance()->getGuiData()->windowSize;
+			std::string countText = "Blocks:" + std::to_string(blockCount);
+			DrawUtils::drawText(vec2_t{ windowSize.x / 2.f, windowSize.y / 2.f + 20.f }, &countText, MC_Color(255, 255, 255), 1.3f);
+			//vec4_t renderPos = {};
+			//DrawUtils::fillRectangle(renderPos, MC_Color(13, 29, 48), 1.f);
+		}
+
+		if (render) {
+			DrawUtils::setColor(0, 0.3705, 1, 1);
+			DrawUtils::drawBox(renderPos, renderPos.add(1), (float)0.5 / (float)1.f, false);
+		}
+	}
 }
--- Horion/Module/Modules/Scaffold.h
+++ Horion/Module/Modules/Scaffold.h
@@ -1,20 +1,38 @@
 #pragma once
 
 #include "Module.h"
+#include "../../DrawUtils.h"
 
 class Scaffold : public IModule {
 private:
-	bool spoof = false;
-	bool staircaseMode = false;
+	//bool spoof = false;
 	bool tryScaffold(vec3_t blockBelow);
-	bool findBlock();
-
+	void findBlock();
+	int length = 5;
+	vec2_t angle;
+	bool showBlockCount = false;
+	void calcCount();
+	int blockCount = 0;
+	bool autoBlock = false;
+	int prevSlot = 0;
+	float horizontalHigh;
+	bool rotations = false;
+	bool render = false;
+	vec3_t renderPos;
+	bool canRender = false;
 public:
+	SettingEnum mode;
+
 	Scaffold();
 	~Scaffold();
 
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
-	virtual void onTick(C_GameMode* gm) override;
+	//virtual void onTick(C_GameMode* gm) override;
 	virtual void onDisable() override;
+	virtual void onPlayerTick(C_Player* player) override;
+	virtual void onSendPacket(C_Packet* packet, bool& cancelSend) override;
+	virtual void onEnable() override;
+	virtual void onPostRender(C_MinecraftUIRenderContext* renderCtx) override;
+	virtual void onGetPickRange() override;
 };
