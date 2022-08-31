2cdeeb2e1c038979f3a80f1cb556c91738edf00c
Coft Anery <AneryCoft13363134145@gmail.com>
2022-08-31 18:19:11 +0800

    容器透视 选定容器透视

--- Horion/Module/Modules/StorageESP.cpp
+++ Horion/Module/Modules/StorageESP.cpp
@@ -3,6 +3,12 @@
 #include "../../DrawUtils.h"
 
 StorageESP::StorageESP() : IModule(0, Category::VISUAL, "ESP for but storage blocks.") {
+	registerBoolSetting("Normal Chest", &normalChest, normalChest);
+	registerBoolSetting("Trapped Chest", &trappedChest, trappedChest);
+	registerBoolSetting("Ender Chest", &enderChest, enderChest);
+	registerBoolSetting("Barrel", &barrel, barrel);
+	registerBoolSetting("Undyed Shulker Box", &undyedShulkerBox, undyedShulkerBox);
+	registerBoolSetting("Shulker Box", &shulkerBox, shulkerBox);
 }
 
 StorageESP::~StorageESP() {
@@ -33,12 +39,20 @@ void StorageESP::onPreRender(C_MinecraftUIRenderContext* renderCtx) {
 		auto mathVect = vec3_t(chest.upper.floor().add(vec3_t(1.f, 1.f, 1.f)).sub(chest.upper));
 		mathVect.y = floor(mathVect.y);
 
-		if (storageID == 54) DrawUtils::setColor(1.f, 1.f, 1.f, math);                     // Normal Chest
-		if (storageID == 146) DrawUtils::setColor(.92f, .14f, .14f, math);                 // Trapped Chest
-		if (storageID == 130) DrawUtils::setColor(0.435294f, 0.215686f, 0.631372f, math);  // Ender Chest
-		if (storageID == 458) DrawUtils::setColor(0.62f, 0.31f, 0.04f, math);                 // Barrel
-		if (storageID == 205) DrawUtils::setColor(.49f, .17f, .95f, math);                 // Undyed Shulker Box
-		if (storageID == 218) DrawUtils::setColor(.08f, .91f, .99f, math);                 // Shulker Box
+		if (storageID == 54 && normalChest)
+			DrawUtils::setColor(1.f, 1.f, 1.f, math);                     //Ïä×Ó
+		else if (storageID == 146 && trappedChest)
+			DrawUtils::setColor(.92f, .14f, .14f, math);                 //ÏÝÚåÏä
+		else if (storageID == 130 && enderChest)
+			DrawUtils::setColor(0.435294f, 0.215686f, 0.631372f, math);  //Ä©Ó°Ïä
+		else if (storageID == 458 && barrel)
+			DrawUtils::setColor(0.62f, 0.31f, 0.04f, math);                 //Í°
+		else if (storageID == 205 && undyedShulkerBox)
+			DrawUtils::setColor(.49f, .17f, .95f, math);                 //Î´È¾É«µÄÇ±Ó°ºÐ
+		else if (storageID == 218 && shulkerBox)
+			DrawUtils::setColor(.08f, .91f, .99f, math);                 //Ç±Ó°ºÐ
+		else
+			continue;
 
 		DrawUtils::drawBox(chest.lower, chest.upper, (float)fmax(0.2f, 1 / (float)fmax(1, g_Data.getLocalPlayer()->eyePos0.dist(chest.lower))), true);  // Fancy math to give an illusion of good esp
 	}
