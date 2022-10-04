5a3355f62073e5222523828dda952e4cac28b0ac
Coft Anery <AneryCoft13363134145@gmail.com>
2022-10-04 16:50:18 +0800

    优化Tracer

--- Horion/Module/Modules/Tracer.h
+++ Horion/Module/Modules/Tracer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Module.h"
+#include "../../DrawUtils.h"
 
 class Tracer : public IModule {
 public:
@@ -9,6 +10,6 @@ public:
 
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
-	void onLevelRender() override;
+	//void onLevelRender() override;
 	void onPreRender(C_MinecraftUIRenderContext* renderCtx) override;
 };
