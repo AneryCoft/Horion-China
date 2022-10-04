3816fd2c874ec15dd96935b92cf97e5a31d85966
Coft Anery <AneryCoft13363134145@gmail.com>
2022-10-04 12:07:35 +0800

    Teleport增强

--- Horion/Module/Modules/Teleport.cpp
+++ Horion/Module/Modules/Teleport.cpp
@@ -1,8 +1,13 @@
 #include "Teleport.h"
 
 Teleport::Teleport() : IModule(0, Category::MISC, "Click a block to teleport to it.") {
+	mode = SettingEnum(this)
+		.addEntry(EnumEntry("Normal", 0))
+		.addEntry(EnumEntry("lerp", 1))
+		.addEntry(EnumEntry("MultiPacket", 2));
+	registerEnumSetting("Mode", &mode, 1);
 	registerBoolSetting("Only Hand", &onlyHand, onlyHand);
-	registerBoolSetting("Push", &bypass, bypass);
+	registerBoolSetting("Only Sneak", &onlySneak, onlySneak);
 }
 
 Teleport::~Teleport() {
@@ -15,6 +20,7 @@ const char* Teleport::getModuleName() {
 void Teleport::onTick(C_GameMode* gm) {
 	if (!GameData::canUseMoveKeys())
 		return;
+
 	if (onlyHand && g_Data.getLocalPlayer()->getSupplies()->inventory->getItemStack(g_Data.getLocalPlayer()->getSupplies()->selectedHotbarSlot)->item != nullptr)
 		return;
 
@@ -36,21 +42,33 @@ void Teleport::onTick(C_GameMode* gm) {
 		hasClicked = false;
 
 	C_GameSettingsInput* input = g_Data.getClientInstance()->getGameSettingsInput();
-	if (shouldTP && GameData::isKeyDown(*input->sneakKey)) {
+
+	if (shouldTP && !(!GameData::isKeyDown(*input->sneakKey) && onlySneak)) {
 		tpPos.y += (gm->player->getPos()->y - gm->player->getAABB()->lower.y) + 1;  // eye height + 1
-		if (bypass) {
-			/*int dist = (int)gm->player->getPos()->dist(tpPos);
+		switch (mode.selected) {
+		case 0:
+		{
+			gm->player->setPos(tpPos);
+		}
+		break;
+		case 1:
+		{
+			float dist = gm->player->getPos()->dist(tpPos);
+			g_Data.getLocalPlayer()->lerpTo(tpPos, vec2_t(1, 1), (int)fmax((int)dist * 0.1, 1));
+		}
+		break;
+		case 2:
+		{
+			int dist = (int)gm->player->getPos()->dist(tpPos);
 			int i = (int)dist / 5;
 			for (int n = 0; n < i; n++) {
 				vec3_t offs = tpPos.sub(*gm->player->getPos()).div(i).mul(n);
 				C_MovePlayerPacket p = C_MovePlayerPacket(g_Data.getLocalPlayer(), gm->player->getPos()->add(offs));
 				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
 			}
-			gm->player->setPos(tpPos);*/
-			float dist = gm->player->getPos()->dist(tpPos);
-			g_Data.getLocalPlayer()->lerpTo(tpPos, vec2_t(1, 1), (int)fmax((int)dist * 0.1, 1));
-		} else
 			gm->player->setPos(tpPos);
+		}
+		}
 		shouldTP = false;
 	}
 }
