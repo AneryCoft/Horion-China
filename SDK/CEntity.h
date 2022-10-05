feba11257d2b8ee9c52e1fee6cd6a3b94ebf6806
MacroModel2019 <33865334+xlfd2018@users.noreply.github.com>
2022-10-05 17:07:32 +0800

    Update CEntity.h

--- SDK/CEntity.h
+++ SDK/CEntity.h
@@ -626,22 +626,16 @@ public:
 	
 	bool isEntityExist() {
 		const auto localPlayer = g_Data.getLocalPlayer();
-		if (loaclPlayer == nullptr) 
+		if (localPlayer == nullptr)
 			return false;
-		__try {
-			localPlayer->canAttack(this, false);
-			return this != nullptr 
-				&& (uintptr_t)this != 0xFFFFFFFFFFFFFCD7
-				&& (uintptr_t)this != 0xFFFFFF00FFFFFF00 
-				&& this != nullptr 
-				&& *(uintptr_t*)this != 0xFFFFFFFFFFFFFCD7 
-				&& *(uintptr_t*)this > 0x6FF000000000 
-				&& *(uintptr_t*)this < 0x800000000000 
-				&& *((int64_t*)this + 1) < 0x6FF000000000 
-				&& *(uintptr_t*)this <= Utils::getBase() + 0x10000000;
+		[localPlayer, this] {
+			__try {
+				localPlayer->canAttack(this, false);
+				return this != nullptr && (uintptr_t)this != 0xFFFFFFFFFFFFFCD7 && (uintptr_t)this != 0xFFFFFF00FFFFFF00 && this != nullptr && *(uintptr_t *)this != 0xFFFFFFFFFFFFFCD7 && *(uintptr_t *)this > 0x6FF000000000 && *(uintptr_t *)this < 0x800000000000 && *((int64_t *)this + 1) < 0x6FF000000000 && *(uintptr_t *)this <= Utils::getBase() + 0x10000000;
 			} __except (EXCEPTION_EXECUTE_HANDLER) {
 				return false;
-		} 
+			}
+		}();
 	}
 };
 #pragma pack(pop)
