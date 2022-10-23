4a0285274c9fafc53820b417bba685ba60de5fe4
Coft Anery <AneryCoft13363134145@gmail.com>
2022-10-23 08:03:15 +0800

    多次发包的InfiniteAura（没有自动寻路 容易被反穿墙限制）

--- Horion/Module/Modules/InfiniteAura.cpp
+++ Horion/Module/Modules/InfiniteAura.cpp
@@ -1,9 +1,13 @@
-#include "InfiniteAura.h"
+﻿#include "InfiniteAura.h"
 
 InfiniteAura::InfiniteAura() : IModule(0, Category::COMBAT, "Killaura but with infinite reach.") {
-	registerBoolSetting("MultiAura", &isMulti, isMulti);
-	registerFloatSetting("Range", &range, range, 15, 100);
-	registerIntSetting("Delay", &delay, delay, 15, 20);
+	mode = SettingEnum(this)
+		.addEntry(EnumEntry("Single", 0))
+		.addEntry(EnumEntry("Multi", 1));
+	registerEnumSetting("Mode", &mode, 0);
+	registerFloatSetting("TPDistance", &tpDistance, tpDistance, 1.f, 20.f);
+	registerFloatSetting("Range", &range, range, 15.f, 128.f);
+	registerIntSetting("CPS", &CPS, CPS, 1, 20);
 }
 
 InfiniteAura::~InfiniteAura() {
@@ -17,18 +21,6 @@ static std::vector<C_Entity*> targetList0;
 
 void findEntities(C_Entity* currentEntity, bool isRegularEntitie) {
 	static auto infiniteAuraMod = moduleMgr->getModule<InfiniteAura>();
-	
-	if (currentEntity == g_Data.getLocalPlayer())  // Skip Local player
-		return;
-
-	if (currentEntity == 0)
-		return;
-
-	if (currentEntity->timeSinceDeath > 0 || currentEntity->damageTime >= 7)
-		return;
-
-	if (FriendList::findPlayer(currentEntity->getNameTag()->getText()))  // Skip Friend
-		return;
 
 	if (!Target::isValidTarget(currentEntity))
 		return;
@@ -41,24 +33,30 @@ void findEntities(C_Entity* currentEntity, bool isRegularEntitie) {
 }
 
 void InfiniteAura::onTick(C_GameMode* gm) {
-
 	//Loop through all our players and retrieve their information
 	targetList0.clear();
 
-	g_Data.forEachEntity(findEntities);
-	Odelay++;
+	g_Data.forEachValidEntity(findEntities);
 
-	if (targetList0.size() > 0 && Odelay >= delay) {
-		g_Data.getLocalPlayer()->swingArm();
+	if (!targetList0.empty() && CPSTime.hasTimedElapsed(1000.f / (float)CPS, true)) {
+		C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
 
-		float calcYaw = (gm->player->yaw + 90) * (PI / 180);
-		float calcPitch = (gm->player->pitch) * -(PI / 180);
+		std::sort(targetList0.begin(), targetList0.end(), [](const C_Entity* lhs, const C_Entity* rhs) {
+			vec3_t localPlayerPos = *g_Data.getLocalPlayer()->getPos();
+			C_Entity* current = const_cast<C_Entity*>(lhs);
+			C_Entity* other = const_cast<C_Entity*>(rhs);
+			return (*current->getPos()).dist(localPlayerPos) < (*other->getPos()).dist(localPlayerPos);
+			}); //距离优先
 
-		float teleportX = cos(calcYaw) * cos(calcPitch) * 3.5f;
-		float teleportZ = sin(calcYaw) * cos(calcPitch) * 3.5f;
-		C_MovePlayerPacket teleportPacket;
+		float calcYaw = (localPlayer->yaw + 90) * (PI / 180);
+		float calcPitch = (localPlayer->pitch) * -(PI / 180);
 
-		if (strcmp(g_Data.getRakNetInstance()->serverIp.getText(), "mco.cubecraft.net") == 0) {
+		float teleportX = cos(calcYaw) * cos(calcPitch) * 3.7f;
+		float teleportZ = sin(calcYaw) * cos(calcPitch) * 3.7f;
+
+		C_MovePlayerPacket movePacket;
+
+		/*if (strcmp(g_Data.getRakNetInstance()->serverIp.getText(), "mco.cubecraft.net") == 0) {
 			vec3_t pos = *g_Data.getLocalPlayer()->getPos();
 
 			C_MovePlayerPacket movePlayerPacket(g_Data.getLocalPlayer(), pos);
@@ -68,26 +66,53 @@ void InfiniteAura::onTick(C_GameMode* gm) {
 
 			movePlayerPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), pos);
 			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePlayerPacket);
-		}
+		}*/
 
-		// Attack all entitys in targetList
-		if (isMulti) {
-			for (int i = 0; i < targetList0.size(); i++) {
-				vec3_t pos = *targetList0[i]->getPos();
-				teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
-				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-				g_Data.getCGameMode()->attack(targetList0[i]);
-				teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
-				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
+		for (auto target : targetList0) {
+			vec3_t targetPos = *target->getPos();
+			vec3_t* localPlayerPos = localPlayer->getPos();
+			vec3_t tpPos = vec3_t(targetPos.x - teleportX, targetPos.y, targetPos.z - teleportZ);
+
+			float times = localPlayerPos->dist(tpPos) / tpDistance; //需要传送的次数
+			for (int n = 0; n < times; n++) {
+				vec3_t offs = tpPos.sub(*localPlayerPos).div(times).mul(n);
+				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&C_MovePlayerPacket(localPlayer, localPlayerPos->add(offs)));
 			}
-		} else {
-			vec3_t pos = *targetList0[0]->getPos();
-			teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), vec3_t(pos.x - teleportX, pos.y, pos.z - teleportZ));
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-			g_Data.getCGameMode()->attack(targetList0[0]);
-			teleportPacket = C_MovePlayerPacket(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&teleportPacket);
-		}
-		Odelay = 0;
+
+			movePacket = C_MovePlayerPacket(localPlayer, tpPos);
+			vec2_t angle = g_Data.getLocalPlayer()->getPos()->CalcAngle(*target->getPos());
+			movePacket.pitch = angle.x;
+			movePacket.headYaw = angle.y;
+			movePacket.yaw = angle.y;
+			//转头
+
+			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePacket);
+			g_Data.getCGameMode()->attack(target);
+			localPlayer->swingArm();
+
+			//回来
+			localPlayerPos = localPlayer->getPos();
+			int backTimes = tpPos.dist(*localPlayerPos) / tpDistance;
+			for (int n = 0; n < backTimes; n++) {
+				vec3_t offs = tpPos.sub(*localPlayerPos).div(backTimes).mul(n);
+				g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&C_MovePlayerPacket(localPlayer, localPlayerPos->add(offs)));
+			}
+
+			movePacket = C_MovePlayerPacket(localPlayer, *localPlayer->getPos());
+			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePacket);
+
+			if (mode.selected == 0) {
+				break;
+			}
+		}	
 	}
+}
+
+void InfiniteAura::onEnable() {
+	if (g_Data.getLocalPlayer() == nullptr) {
+		this->setEnabled(false);
+	}
+
+	CPSTime.resetTime();
+	//计时器初始化
 }
--- Horion/Module/Modules/InfiniteAura.h
+++ Horion/Module/Modules/InfiniteAura.h
@@ -2,19 +2,21 @@
 
 #include "Module.h"
 #include "../ModuleManager.h"
+#include "../../../Utils/TimerUtil.h"
 
 class InfiniteAura : public IModule {
 private:
-	int delay = 15;
-	int Odelay = 0;
-	bool isMulti = false;
-
+	int CPS = 5;
+	TimerUtil CPSTime;
+	float tpDistance = 5.f;
+	SettingEnum mode;
 public:
-	float range = 15;
+	float range = 100;
 	InfiniteAura();
 	~InfiniteAura();
 	
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
+	virtual void onEnable() override;
 };
