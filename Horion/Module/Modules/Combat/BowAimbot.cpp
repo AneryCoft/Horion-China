0f98b2d6eea708e254764ff651387772b0cd4e35
Coft Anery <AneryCoft13363134145@gmail.com>
2022-11-26 15:28:42 +0800

    弓自瞄的升级

--- Horion/Module/Modules/Combat/BowAimbot.cpp
+++ Horion/Module/Modules/Combat/BowAimbot.cpp
@@ -1,37 +1,37 @@
 #include "BowAimbot.h"
 
-#include "../../../../Utils/Target.h"
-#include "../../../DrawUtils.h"
-
-std::vector<C_Entity*> targetList;
-
 BowAimbot::BowAimbot() : IModule(0, Category::COMBAT, "Aimbot, but for bows.") {
+	priority = SettingEnum(this)
+		.addEntry(EnumEntry("Distance", 0))
+		.addEntry(EnumEntry("Angle", 1));
+	registerEnumSetting("Priority", &priority, 0);
+	registerFloatSetting("Rotation FOV", &FOV, FOV, 10.f, 180.f);
 	registerBoolSetting("Silent", &silent, silent);
 	registerBoolSetting("Predict", &predict, predict);
+	registerBoolSetting("ThroughBlock", &throughBlock, throughBlock);
 	registerBoolSetting("Visualize", &visualize, visualize);
 }
 
 BowAimbot::~BowAimbot() {
 }
 
-struct CompareTargetEnArray {
-	bool operator()(C_Entity* lhs, C_Entity* rhs) {
-		C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
-		return (*lhs->getPos()).dist(*localPlayer->getPos()) < (*rhs->getPos()).dist(*localPlayer->getPos());
-	}
-};
-
 const char* BowAimbot::getModuleName() {
 	return ("BowAimbot");
 }
+
+static std::vector<C_Entity*> targetList;
 
 void findTargets(C_Entity* currentEntity, bool isRegularEntitie) {
 	if (!Target::isValidTarget(currentEntity))
 		return;
 
+	static auto bowAimbotMod = moduleMgr->getModule<BowAimbot>();
+	if (!bowAimbotMod->throughBlock && !g_Data.getLocalPlayer()->canSee(currentEntity))
+		return;
+
 	float dist = (*currentEntity->getPos()).dist(*g_Data.getLocalPlayer()->getPos());
 
-	if (dist < 130) {
+	if (dist < 160) {
 		targetList.push_back(currentEntity);
 	}
 }
@@ -52,18 +52,46 @@ void BowAimbot::onPreRender(C_MinecraftUIRenderContext* renderCtx) {
 	g_Data.forEachEntity(findTargets);
 
 	if (!targetList.empty()) {
-		std::sort(targetList.begin(), targetList.end(), CompareTargetEnArray());
-		vec3_t origin = g_Data.getLocalPlayer()->eyePos0;  // TODO: sort list
+		if (priority.selected == 0) {
+			struct DistanceCompare {
+				bool operator()(C_Entity* lhs, C_Entity* rhs) {
+					C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+					return (*lhs->getPos()).dist(*localPlayer->getPos()) < (*rhs->getPos()).dist(*localPlayer->getPos());
+				}
+			};
+
+			std::sort(targetList.begin(), targetList.end(), DistanceCompare());
+		}
+		else if (priority.selected == 1) {
+			struct AngleCompare {
+				bool operator()(C_Entity* lhs, C_Entity* rhs) {
+					C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+					vec2_t angle = localPlayer->getPos()->CalcAngle(*lhs->getPos());
+					vec2_t angle2 = localPlayer->getPos()->CalcAngle(*rhs->getPos());
+					vec2_t appl = angle.sub(localPlayer->viewAngles).normAngles();
+					vec2_t appl2 = angle2.sub(localPlayer->viewAngles).normAngles();
+					//logF("appl = %f appl2 = %f", abs(appl.y), abs(appl2.y));
+					return abs(appl.y) < abs(appl2.y);
+				}
+			};
+
+			std::sort(targetList.begin(), targetList.end(), AngleCompare());
+		}
+
+		vec3_t origin = *localPlayer->getPos();
 		C_Entity* entity = targetList[0];
 		vec3_t pos = entity->aabb.centerPoint();
+
 		if (predict) {
 			vec3_t velocity = entity->getPos()->sub(*entity->getPosOld());
 			velocity.x *= origin.dist(pos) / 2.f;
 			velocity.z *= origin.dist(pos) / 2.f;
 			pos = pos.add(velocity);
 		}
-		if(visualize)
+
+		if (visualize)
 			DrawUtils::drawBox(pos.sub(0.5), pos.add(0.5), 0.3f, true);
+
 		pos = pos.sub(origin);
 		float yaw = (atan2f(pos.z, pos.x) * DEG_RAD) - 90;
 		float len = pos.magnitudexz();
@@ -71,22 +99,23 @@ void BowAimbot::onPreRender(C_MinecraftUIRenderContext* renderCtx) {
 		float tmp = 1 - g * (g * (len * len) + 2 * pos.y);
 		float pitch = DEG_RAD * -atanf((1 - sqrtf(tmp)) / (g * len));
 
-		if (silent) {
+		if (pitch < 89.9f && pitch > -89.9f) {
 			angle = vec2_t(pitch, yaw);
-			C_MovePlayerPacket p(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
-			p.pitch = angle.x;
-			p.yaw = angle.y;
-			p.headYaw = angle.y;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&p);
-		} else {
-			if (pitch < 89 && pitch > -89) {
-				vec2_t angles = vec2_t(pitch, yaw);
+			vec2_t appl = angle.sub(localPlayer->viewAngles).normAngles();
+			appl.x = -appl.x;
+			if ((appl.y < FOV && appl.y > -FOV)) {
+				if (silent) {
+					C_MovePlayerPacket packet(localPlayer, *g_Data.getLocalPlayer()->getPos());
+					packet.pitch = angle.x;
+					packet.yaw = angle.y;
+					packet.headYaw = angle.y;
+					g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&packet);
+				}
+				else {
+					appl = appl.div(7);  // Smooth dat boi
 
-				vec2_t appl = angles.sub(localPlayer->viewAngles).normAngles();
-				appl.x = -appl.x;
-				appl = appl.div(7);  // Smooth dat boi
-
-				localPlayer->applyTurnDelta(&appl);
+					localPlayer->applyTurnDelta(&appl);
+				}
 			}
 		}
 	}
--- Horion/Module/Modules/Combat/BowAimbot.h
+++ Horion/Module/Modules/Combat/BowAimbot.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../Module.h"
+#include "../../../DrawUtils.h"
+#include "../../ModuleManager.h"
 
 class BowAimbot : public IModule {
 private:
@@ -8,14 +10,17 @@ private:
 	bool predict = false;
 	bool visualize = false;
 	vec2_t angle;
+	float FOV = 180.f;
+	SettingEnum priority;
 
 public:
+	bool throughBlock = false;
+
 	BowAimbot();
 	~BowAimbot();
 
-	virtual void onPreRender(C_MinecraftUIRenderContext* renderCtx) override;
-	virtual void onSendPacket(C_Packet* packet, bool& cancelSend) override;
-
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
+	virtual void onPreRender(C_MinecraftUIRenderContext* renderCtx) override;
+	virtual void onSendPacket(C_Packet* packet, bool& cancelSend) override;
 };
