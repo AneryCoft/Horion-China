7a3aa6460f914544b81dd363c194700f37eef7ed
Coft Anery <AneryCoft13363134145@gmail.com>
2022-12-06 14:42:51 +0800

    上帝模式优化++

--- Horion/Module/Modules/Misc/Godmode.cpp
+++ Horion/Module/Modules/Misc/Godmode.cpp
@@ -12,30 +12,9 @@ const char* Godmode::getModuleName() {
 
 void Godmode::onTick(C_GameMode* gm) {
 	C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
-	if(localPlayer == nullptr) return;
-	vec3_t localPlayerPos = *localPlayer->getPos();
-	float posY = localPlayerPos.y + 0.8f;
+	if (localPlayer == nullptr)
+		return;
 
-		C_MovePlayerPacket movePacket;
-		for (; posY >= localPlayerPos.y; posY -= 0.62f) {
-			movePacket.Position = vec3_t(localPlayerPos.x + 6666, posY + 6666, localPlayerPos.z + 6666);  // send position to server
-			// localPlayer->setPos(p.pos);
-			if (posY <= localPlayerPos.y)
-				posY = localPlayerPos.y + 0.2;
-		}
-		g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePacket);
-
-		if (localPlayer->onGround) {
-			C_PlayerActionPacket actionPacket;
-			actionPacket.action = 8;  // Send Jump Action
-			actionPacket.entityRuntimeId = localPlayer->entityRuntimeId;
-			g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&actionPacket);
-	}
-}
-
-void Godmode::onSendPacket(C_Packet* packet, bool& cancelSend) {
-	if (packet->isInstanceOf<C_MovePlayerPacket>()) {
-		C_MovePlayerPacket* movePacket = reinterpret_cast<C_MovePlayerPacket*>(packet);
-		movePacket->onGround = true;
-	}
+	C_MovePlayerPacket movePacket(localPlayer, localPlayer->getPos()->add(99999.f));
+	g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePacket);
 }
--- Horion/Module/Modules/Misc/Godmode.h
+++ Horion/Module/Modules/Misc/Godmode.h
@@ -12,5 +12,4 @@ public:
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 	virtual void onTick(C_GameMode* gm) override;
-	virtual void onSendPacket(C_Packet* packet, bool& cancelSend) override;
 };
