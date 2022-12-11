ec577c867ec121cef923f43e1b6e2795be15f935
Coft Anery <AneryCoft13363134145@gmail.com>
2022-12-11 10:48:18 +0800

    Update Step.cpp

--- Horion/Module/Modules/Movement/Step.cpp
+++ Horion/Module/Modules/Movement/Step.cpp
@@ -13,6 +13,12 @@ const char* Step::getModuleName() {
 
 void Step::onTick(C_GameMode* gm) {
 	gm->player->stepHeight = height;
+
+	/*if (gm->player->walkingIntoObj) {
+		C_MovePlayerPacket movePacket;
+		movePacket.Position = gm->player->getPos()->add(0.f, 1.2f, 0.f);
+		g_Data.getClientInstance()->loopbackPacketSender->sendToServer(&movePacket);
+	}*/
 }
 void Step::onDisable() {
 	if (g_Data.getLocalPlayer() != nullptr)
