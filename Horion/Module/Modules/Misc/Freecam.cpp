6b444a2d1c2afc32b38934d02b12e64a80255251
Coft Anery <AneryCoft13363134145@gmail.com>
2022-12-11 10:49:33 +0800

    更合法的FreeCam

--- Horion/Module/Modules/Misc/Freecam.cpp
+++ Horion/Module/Modules/Misc/Freecam.cpp
@@ -11,36 +11,50 @@ const char* Freecam::getModuleName() {
 	return ("Freecam");
 }
 
+void Freecam::onEnable() {
+	auto localPlayer = g_Data.getLocalPlayer();
+	if (localPlayer != nullptr) {
+		oldPos = *localPlayer->getPos();
+		oldOffset = localPlayer->aabb.upper.sub(localPlayer->aabb.lower);
+
+		pos = oldPos;
+		pitch = localPlayer->pitch;
+		yaw = localPlayer->yaw;
+	}
+}
+
+void Freecam::onDisable() {
+	auto plr = g_Data.getLocalPlayer();
+	if (plr) {
+		plr->setPos(oldPos);
+		//*g_Data.getClientInstance()->minecraft->timer = 20.f;
+		plr->aabb.upper = plr->aabb.lower.add(oldOffset);
+	}
+}
+
 void Freecam::onTick(C_GameMode* gm) {
 	gm->player->fallDistance = 0.f;
 	gm->player->velocity = vec3_t(0, 0, 0);
 	gm->player->aabb.upper.y = gm->player->aabb.lower.y - 1.8f;
 }
 
-void Freecam::onEnable() {
-	if (g_Data.getLocalPlayer() != nullptr) {
-		oldPos = *g_Data.getLocalPlayer()->getPos();
-		oldOffset = g_Data.getLocalPlayer()->aabb.upper.sub(g_Data.getLocalPlayer()->aabb.lower);
-	}
-}
-
 void Freecam::onMove(C_MoveInputHandler* input) {
 	auto player = g_Data.getLocalPlayer();
-	if (player == nullptr) 
+	if (player == nullptr)
 		return;
 
-	vec2_t moveVec2d = {input->forwardMovement, -input->sideMovement};
+	vec2_t moveVec2d = { input->forwardMovement, -input->sideMovement };
 	bool pressed = moveVec2d.magnitude() > 0.01f;
 
 	float calcYaw = (player->yaw + 90) * (PI / 180);
 	vec3_t moveVec;
 	float c = cos(calcYaw);
 	float s = sin(calcYaw);
-	moveVec2d = {moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c};
+	moveVec2d = { moveVec2d.x * c - moveVec2d.y * s, moveVec2d.x * s + moveVec2d.y * c };
 	moveVec.x = moveVec2d.x * speed;
 	moveVec.y = player->velocity.y;
 	moveVec.z = moveVec2d.y * speed;
-	if (pressed) 
+	if (pressed)
 		player->lerpMotion(moveVec);
 	//C_MovePlayerPacket p(g_Data.getLocalPlayer(), *g_Data.getLocalPlayer()->getPos());
 	if (input->isJumping) {
@@ -51,15 +65,6 @@ void Freecam::onMove(C_MoveInputHandler* input) {
 	}
 }
 
-void Freecam::onDisable() {
-	auto plr = g_Data.getLocalPlayer();
-	if (plr) {
-		plr->setPos(oldPos);
-		*g_Data.getClientInstance()->minecraft->timer = 20.f;
-		plr->aabb.upper = plr->aabb.lower.add(oldOffset);
-	}
-}
-
 void Freecam::onSendPacket(C_Packet* packet, bool& cancelSend) {
 	if (packet->isInstanceOf<C_PlayerActionPacket>()) {
 		C_PlayerActionPacket* packets = reinterpret_cast<C_PlayerActionPacket*>(packet);
@@ -67,8 +72,14 @@ void Freecam::onSendPacket(C_Packet* packet, bool& cancelSend) {
 			cancelSend = true;
 		}
 	}
-
-	if (packet->isInstanceOf<C_MovePlayerPacket>()|| packet->isInstanceOf<PlayerAuthInputPacket>()) {
+	else if (packet->isInstanceOf<C_MovePlayerPacket>()) {
 		cancelSend = true;
+	}
+	else if (packet->isInstanceOf<PlayerAuthInputPacket>()) {
+		PlayerAuthInputPacket* packets = reinterpret_cast<PlayerAuthInputPacket*>(packet);
+		packets->pos = pos;
+		packets->pitch = pitch;
+		packets->yaw = yaw;
+		packets->velocity = velocity;
 	}
 }
--- Horion/Module/Modules/Misc/Freecam.h
+++ Horion/Module/Modules/Misc/Freecam.h
@@ -7,6 +7,12 @@ private:
 	vec3_t oldPos;
 	vec3_t oldOffset = {};
 	float speed = 0.325f;
+
+	vec3_t pos;
+	float pitch;
+	float yaw;
+	vec3_t velocity = vec3_t(0.f, 0.f, 0.f);
+	//PlayerAuthInputPacket²ÎÊý±£´æ
 public:
 	Freecam();
 	~Freecam();
